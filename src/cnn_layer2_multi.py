e76d071b6590cd83eed63ae559bb0d026bfac495
hyunghunny <hyunghunny@gmail.com>
2016-07-25 16:06:07 +0900

    reduce test image size to avoid resource error

--- src/cnn_layer2_multi.py
+++ src/cnn_layer2_multi.py
@@ -33,7 +33,7 @@ from util import CSVLogger
 n_input = 784 # MNIST data input (img shape: 28*28)
 n_classes = 10 # MNIST total classes (0-9 digits)
 neurons = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-train_images = [256, 512, 2048, 8092]
+train_images = [256, 512, 2048]
 total_gpu = 3
 log_path = '../log/mnist-cnn.csv'
 
