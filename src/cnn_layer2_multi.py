9c29f6c015016c1252584997c2bcd536ac5e2502
hyunghunny <hyunghunny@gmail.com>
2016-07-25 12:11:28 +0900

    fix indentation

--- src/cnn_layer2_multi.py
+++ src/cnn_layer2_multi.py
@@ -232,14 +232,14 @@ def train(layer1_out=None, layer2_out=None, fully=None) :
 			            train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=i, conv_2_output=j, fully_output=k)
       				    gpu_id += 1
     	elif layer2_out is None:
-                for j in neurons:
-                    for k in neurons:			
-    	                train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=layer1_out, conv_2_output=j, fully_output=k)
-                        gpu_id += 1
-    	elif fully is None:
-                for k in neurons:
-    		        train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=layer1_out, conv_2_output=layer2_out, fully_output=k)
+            for j in neurons:
+                for k in neurons:			
+	                train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=layer1_out, conv_2_output=j, fully_output=k)
                     gpu_id += 1
+    	elif fully is None:
+            for k in neurons:
+		        train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=layer1_out, conv_2_output=layer2_out, fully_output=k)
+                gpu_id += 1
         else:
             train_mnist_nn(logger, dataset, conv_net_2, main_gpu=gpu_id, conv_1_output=layer1_out, conv_2_output=layer2_out, fully_output=fully)
     except:
